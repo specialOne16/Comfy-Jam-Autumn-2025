2a813ceff5f83ef8c55e45bfbf80c7571a556e81
Jun <jundi2303@gmail.com>
2025-11-05 08:46:58 +0700

    Move theme to project level and add character animation

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,9 @@ class_name Player
 
 
 @export var speed = 400
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 var pause_movement = false
 
 
@@ -32,5 +35,28 @@ func place(map_position: Vector2i):
 func _physics_process(delta):
 	get_input()
 	
+	update_animation()
+	
 	var collision = move_and_collide(velocity * delta)
 	if collision: handle_collision(collision, delta)
+
+func update_animation():
+	if velocity == Vector2.ZERO:
+		sprite.play("default")
+		return
+	
+	var direction = velocity.angle()
+	print(direction, PI/4)
+	
+	if direction >= -PI/4 and direction <= PI/4:
+		sprite.scale.x = -0.2
+		sprite.play("move_side")
+	elif direction > PI/4 and direction < 3*PI/4:
+		sprite.scale.x = 0.2
+		sprite.play("move_down")
+	elif direction < -PI/4 and direction > -3*PI/4:
+		sprite.scale.x = 0.2
+		sprite.play("move_up")
+	else:
+		sprite.scale.x = 0.2
+		sprite.play("move_side")
